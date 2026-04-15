17e055f0468e3c744dea6ce16fa8c4f8a3055082
atharvchaudhari76-blip <atharvchaudhari76@gmail.com>
2026-04-15 11:24:43 +0530

    Remove Netlify configuration and static site generation

--- freeze.py
+++
@@ -1,9 +0,0 @@
-from flask_frozen import Freezer
-from main import app
-
-# Ensure links are relative for GitHub Pages subpaths
-app.config['FREEZER_RELATIVE_URLS'] = True
-freezer = Freezer(app)
-
-if __name__ == '__main__':
-    freezer.freeze()
